1c4400bc9f28f62d0d53f17be6eddeb88b64546e
Raphael Baltazar <raphaelbaltazar22@gmail.com>
2018-03-20 10:11:02 -0700

    forgot to push this

--- app/models/inventory.rb
+++ app/models/inventory.rb
@@ -1,2 +1,14 @@
+# == Schema Information
+#
+# Table name: inventories
+#
+#  id         :integer          not null, primary key
+#  name       :string
+#  price      :float
+#  quantity   :integer
+#  created_at :datetime         not null
+#  updated_at :datetime         not null
+#
+
 class Inventory < ApplicationRecord
 end
--- test/models/inventory_test.rb
+++ test/models/inventory_test.rb
@@ -1,3 +1,15 @@
+# == Schema Information
+#
+# Table name: inventories
+#
+#  id         :integer          not null, primary key
+#  name       :string
+#  price      :float
+#  quantity   :integer
+#  created_at :datetime         not null
+#  updated_at :datetime         not null
+#
+
 require 'test_helper'
 
 class InventoryTest < ActiveSupport::TestCase
