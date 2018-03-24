126570b7b2850659fa4a7ac85f3cd7e19a535ece
Raphael Baltazar <raphaelbaltazar22@gmail.com>
2018-03-24 10:40:41 -0700

    changed root page from /views/homepage/homepage to /views/items/show

--- config/routes.rb
+++ config/routes.rb
@@ -5,7 +5,7 @@ Rails.application.routes.draw do
   resources :rewards
   get 'homepage/home'
 
-  root to: 'homepage#home'
+  root to: 'items#index'
   devise_for :users
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
