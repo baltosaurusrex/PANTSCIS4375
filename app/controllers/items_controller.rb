9d6f2c2d3e53d03096f5d0dcf7dff1e1e196c8eb
Raphael Baltazar <raphaelbaltazar22@gmail.com>
2018-03-28 13:41:00 -0700

    added a bit of authentication. WIP. added comments in /views/homepage for Raymond

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -1,6 +1,8 @@
 class ItemsController < ApplicationController
   before_action :set_item, only: [:show, :edit, :update, :destroy]
 
+  skip_before_action :authenticate_user!, :only=> [:index,:show]
+
   # GET /items
   # GET /items.json
   def index
