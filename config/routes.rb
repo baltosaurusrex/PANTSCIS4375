330064a6a8c13f87ba1b8187daf4a6348b955f62
Raphael Baltazar <raphaelbaltazar22@gmail.com>
2018-03-30 14:20:09 -0700

    rearranged containers. made homepage root again.

--- config/routes.rb
+++ config/routes.rb
@@ -7,9 +7,12 @@ Rails.application.routes.draw do
   resources :items
   resources :line_items
 
-  get 'homepage/home'
+  #swapping between items#index and homepage#home while home is being built
+  get 'homepage/home' #views/homepage/home
+  #root to: 'items#index' #views/items/index
 
-  root to: 'items#index'
+  root to: 'homepage#home'
+
   devise_for :users
   resources :users, :only => [:show]
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
